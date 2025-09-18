56c9bdc50b6f19e469858a45843f6d3c6f4e484f
IT2166070100 <66070100@kmitl.ac.th>
2025-09-18 23:01:24 +0700

    fix 6.2

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -4,7 +4,7 @@ def downcase_it(s):
     return s.lower()
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1: 
+    if len(sys.argv) == 1:
         print("none")
     else:
         for arg in sys.argv[1:]:
